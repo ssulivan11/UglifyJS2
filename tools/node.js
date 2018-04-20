5415d22f424f42c9d349e3dfd2a3dec5c37056f4
Scott Sullivan <Scott.Sullivan@argos.co.uk>
2018-04-20 13:41:41 +0100

    webpack fix

--- tools/node.js
+++ tools/node.js
@@ -1,20 +1,7 @@
 var fs = require("fs");
 
 var UglifyJS = exports;
-var FILES = UglifyJS.FILES = [
-    "../lib/utils.js",
-    "../lib/ast.js",
-    "../lib/parse.js",
-    "../lib/transform.js",
-    "../lib/scope.js",
-    "../lib/output.js",
-    "../lib/compress.js",
-    "../lib/sourcemap.js",
-    "../lib/mozilla-ast.js",
-    "../lib/propmangle.js",
-    "../lib/minify.js",
-    "./exports.js",
-].map(function(file){
+var FILES = UglifyJS.FILES = [].map(function(file){
     return require.resolve(file);
 });
 
